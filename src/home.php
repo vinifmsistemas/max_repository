02257f4afe25fe09ef07c34c10e0cb38cbc5ff2d
theg4k <giovanna.janiak@flexinterativa.com.br>
2023-02-19 21:28:57 -0300

    a
    a

--- src/home.php
+++ src/home.php
@@ -1,5 +1,5 @@
 <!-- Part 01 -->
-<div class="background01">
+<div class="background01 w-100 h-100">
   <div id="background01-carousel" class="carousel slide" data-bs-ride="carousel" data-interval="false">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#background01-carousel" data-bs-slide-to="0" class="active"
@@ -28,7 +28,7 @@
 </div>
 
 <!-- Part 02 -->
-<div class="background02">
+<div class="background02 w-100">
   <div id="background02-carousel" class="carousel slide" data-bs-ride="carousel" data-interval="0">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#background02-carousel" data-bs-slide-to="0" class="active"
@@ -38,59 +38,35 @@
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <h1 class="proposta">
-          SOMOS OUSADOS E QUEREMOS SER O PRIMEIRO DISTRITO GAMER DO BRASIL,<br>
-          DESENVOLVENDO ACESSO DEMOCRÁTICO PARA TODAS AS PESSOAS, SERES E SABERES</h1>
-        <div class="caixa-caixas-p-evento">
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVER SEU INGRESSO PARA O MAIOR<br>
-              EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              DO BRASIL
-            </p>
-          </div>
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVER SEU INGRESSO PARA O MAIOR<br>
-              EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              DO BRASIL
-            </p>
-          </div>
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVER SEU INGRESSO PARA O MAIOR<br>
-              EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              DO BRASIL
-            </p>
+        <div class="row mx-5 pb-4">
+          <div class="col-md-12 text-center">
+            <h3 class="background02-text mt-5">
+              SOMOS OUSADOS E QUEREMOS SER O PRIMEIRO DISTRITO GAMER DO BRASIL,<br>
+              DESENVOLVENDO ACESSO DEMOCRÁTICO PARA TODAS AS PESSOAS, SERES E SABERES
+            </h3>
           </div>
         </div>
-      </div>
-
-      <div class="carousel-item">
-        <h1 class="proposta">
-          SOMOS OUSADOS E QUEREMOS SER O PRIMEIRO DISTRITO GAMER DO BRASIL,<br>
-          DESENVOLVENDO ACESSO DEMOCRÁTICO PARA TODAS AS PESSOAS, SERES E SABERES</h1>
-        <div class="caixa-caixas-p-evento">
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVE SEU INGRESSO PARA O MAIOR<br>
+        <div class="row mx-5 mt-5">
+          <div class="col-md-4 text-center">
+            <h4 class="background02-text">DIAS 29 3 20 DE ABRIL.<br>
+              RESERVER SEU INGRESSO PARA O MAIOR<br>
               EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              NO BRASIL
-            </p>
+              DO BRASIL
+            </h4>
           </div>
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVE SEU INGRESSO PARA O MAIOR<br>
+          <div class="col-md-4 text-center">
+            <h5 class="background02-text">DIAS 29 3 20 DE ABRIL.<br>
+              RESERVER SEU INGRESSO PARA O MAIOR<br>
               EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              NO BRASIL
-            </p>
+              DO BRASIL
+            </h5>
           </div>
-          <div class="caixa-p-evento">
-            <p class="p-evento">DIAS 29 3 20 DE ABRIL.<br>
-              RESERVE SEU INGRESSO PARA O MAIOR<br>
+          <div class="col-md-4 text-center">
+            <h5 class="background02-text">DIAS 29 3 20 DE ABRIL.<br>
+              RESERVER SEU INGRESSO PARA O MAIOR<br>
               EVENTO DO UNIVERSO DE GENSHIN IMPACT<br>
-              NO BRASIL
-            </p>
+              DO BRASIL
+            </h5>
           </div>
         </div>
       </div>
@@ -99,7 +75,7 @@
 </div>
 
 <!-- Part 03 -->
-<div class="background03">
+<div class="background03 w-100 h-100">
   <div class="background03-container-background">
     <div class="background03-container-text">
       <h6>asdokasodkaoskdoaskdokaksd</h6>
@@ -111,14 +87,14 @@
   </div>
 </div>
 
-<!--Videos campeonatos-->
-<div class="videos">
-  <div class="container-videos">
-
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
+<!-- Part 04 -->
+<div class="background04 w-100 h-100">
+  <div class="background04-container-background">
+    <!-- Tab buttons -->
+    <ul class="nav nav-tabs" id="background04-tab" role="tablist">
       <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button"
-          role="tab" aria-controls="home" aria-selected="true">Home</button>
+        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#max-league" type="button"
+          role="tab" aria-controls="home" aria-selected="true">Max League</button>
       </li>
       <li class="nav-item" role="presentation">
         <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button"
@@ -129,13 +105,32 @@
           role="tab" aria-controls="contact" aria-selected="false">Contact</button>
       </li>
     </ul>
-    <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <img class="w-100 imagem-abc" src="/assets/img/tabiniciomax.png">
+    <!-- Tab content -->
+    <div class="tab-content" id="background04-tab-content">
+      <div class="tab-pane fade show active" id="max-league" role="tabpanel" aria-labelledby="home-tab">
+        <div class="row mx-0">
+          <div class="col-md-8 px-0">
+            <video class="w-100 h-100" src="https://www.youtube.com/watch?v=ewRDuEroMGI&ab_channel=Bronziocre"
+              controls></video>
+          </div>
+          <div class="col-md-4 px-0">
+            <img src="./assets//img/tabiniciomax-img.png" alt="">
+          </div>
+        </div>
+        <div class="row mx-0">
+          <div class="col-md-4 px-0">
+            <img src="./assets//img/tabiniciomax-img.png" alt="">
+          </div>
+          <div class="col-md-4 px-0">
+            <img src="./assets//img/tabiniciomax-img.png" alt="">
+          </div>
+          <div class="col-md-4 px-0">
+            <img src="./assets//img/tabiniciomax-img.png" alt="">
+          </div>
+        </div>
       </div>
       <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
       <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
     </div>
-
   </div>
 </div>
